4f115d2958c1d358a5959c7fb26b395245829daa
kigi <kigi.chang@gmail.com>
2025-01-08 14:03:58 +0800

    加判斷 <|im_end|>

--- little-wine/src/main.rs
+++ little-wine/src/main.rs
@@ -24,15 +24,12 @@ fn main() -> Result<()> {
     let data: Vec<String> = lst
         .iter()
         .map(|wine| {
-            // println!("{}", wine.profile);
-            format!("{}", wine.profile)
+            let mut buf = Vec::new();
+            serde_json::to_writer(&mut buf, &wine.profile)
+                .map(move |_| String::from_utf8_lossy(&buf).to_string())
+                .map_err(E::msg)
         })
-        .collect::<Vec<String>>();
-    // println!("data:{:?}", data);
-    // {
-    //     let a = std::fs::File::create("data.json")?;
-    //     serde_json::to_writer(a, &data)?;
-    // }
+        .collect::<Result<Vec<String>>>()?;
 
     const MODLE_NAME: &str = "BAAI/bge-m3";
 
@@ -135,7 +132,7 @@ fn main() -> Result<()> {
     //println!("{}", prompt);
     //let prompt = r#"你是位葡萄酒愛好者，我會提供酒友的問題，以及推薦的葡萄酒資料。請你依推薦的葡萄酒資料，撰寫一段評論。問題是：「推薦適合牛排的葡萄酒？」。以下是推薦的葡萄酒的 JSON 資料：[{"酒名":"Beringer Founders' Estate Cabernet Sauvignon","簡介":"甜美的美國入門搭餐酒","參考價格":549,"產區":"California","國家":"美國","酒莊(廠)":"Beringer","葡萄品種":"Cabernet Sauvignon(100%)","購買通路":"好市多","年份":"2021年","酒精濃度":0.138,"甜度":2,"酸度":3,"飽滿度":5,"適合搭配食物":"牛排","品酒筆記":"帶有黑棗、黑李與蜜餞風味，口感甜美，單寧柔順的酒款。適合搭配牛排，也很適合很討厭酸澀葡萄酒的初學者。3.5顆星。"},{"酒名":"Motif Cabernet Sauvignon red hills","簡 介":"甜美柔滑的美國酒","參考價格":299,"產區":"Red hills lake county AVA","國家":"美國","酒莊(廠)":"Motif","葡萄品種":"Cabernet Sauvignon(100%)","購買通路":"好市多","年份":"2022年","酒精濃度":0.145,"甜度":1,"酸度":4,"飽滿度":5,"適合搭配食 物":"牛排","品酒筆記":"來自於美國 Red hills lake county AVA 產區的卡本內蘇維翁，帶有甜美的黑李子蜜餞風味，口感柔滑，單寧柔順，談不上什麼複雜度，就是款開瓶就好喝，又不會過於甜膩的日常餐酒，適合搭配牛排也很適合新手的入門酒款。4顆星。"},{"酒名":"Ségla 二軍紅酒","簡介":"細緻優雅的波爾多二軍紅酒","參考價格":2000,"產區":"Margaux","國家":"法國","酒莊(廠)":"Chateau Rauzan-Ségla","葡萄品種":"波爾多混釀(100%)","購買通路":"法蘭絲","年份":"2017年","酒精濃度":0.135,"甜度":1,"酸度":4,"飽滿度":5,"適合搭配食物":"牛排","品酒筆記":"波爾多二級酒莊 Segla 二軍紅酒，帶有明顯的木桶香氣混合黑色水果香氣，口感優雅，單寧適中搭配肋眼、紐約客牛排相當不錯。"},{"酒名":"Wynns Black Label Cabernet Sauvignon","簡介":"飽滿圓滑有層次的澳洲 卡本內蘇維翁","參考價格":1800,"產區":"Coonawarra","國家":"澳洲","酒莊(廠)":"Wynn","葡萄品種":"Cabernet Sauvignon(100%)","購買通路":"Wine O'Clock,LOCA Co","年份":"2019年","酒精濃度":0.138,"甜度":1,"酸度":4,"飽滿度":5,"適合搭配食物":"牛排","品酒筆記":"嚴選酒莊位於庫納瓦拉產區terra rossa紅土葡萄園中，20%-25%最頂級的卡本內蘇維濃 (Cabernet Sauvignon)葡萄釀造。在法國新(30%)及舊橡木桶中陳放16個月。帶有黑色水果與木桶風味，口感飽滿圓滑，單寧適中，適合搭配牛排。"},{"酒名":"Wente Merlot","簡介":"適合牛排或聚餐的美國Wente Merlot 紅酒","參考價格":619,"產區":"Central coast","國家":"美國","酒莊(廠)":"Wente","葡萄品種":"Ｍerlot(100%)","購買通路":"橡木桶洋酒","年份":"2021年","酒精濃度":0.135,"甜度":1,"酸度":4,"飽滿度":5,"適合搭配食物":"牛排","品酒筆記":"Wente 也是橡木桶架上的長年品項，自己也是喝很多次。\n\n小資對於加州 Merlot的印象很好，不會太柔軟，很有自己的風格。 這款來自於 加州 Central coast 的 Merlot，帶有黑莓、可可以及適切的桶位，剛剛好的單寧，有架構不會過澀， 這種算是中價位，適合帶去聚餐、吃牛排，也是我去橡木桶看到價錢優惠，就會毫不思索結帳的酒款，建議醒酒30分鐘。4顆星推薦。"}]"#;
     let msg = format!("<|im_start|>system\n你是位葡萄酒愛好者，你會依照酒友的問題，推薦合適的葡萄酒。<|im_end|>\n{}", prompt);
-
+    println!("產生答案中...");
     generate(&msg, &device)?;
     Ok(())
 }
@@ -233,6 +230,11 @@ impl TextGeneration {
             Some(token) => token,
             None => anyhow::bail!("cannot find the <|endoftext|> token"),
         };
+
+        let eos_token2 = match self.tokenizer.get_token("<|im_end|>") {
+            Some(token) => token,
+            None => anyhow::bail!("cannot find the <|im_end|> token"),
+        };
         let start_gen = std::time::Instant::now();
         for index in 0..sample_len {
             let context_size = if index > 0 { 1 } else { tokens.len() };
@@ -256,6 +258,20 @@ impl TextGeneration {
             tokens.push(next_token);
             generated_tokens += 1;
             if next_token == eos_token {
+                //println!("next_token:{}", next_token);
+                break;
+            }
+            if index > 0 && next_token == eos_token2 {
+                // 加判斷 <|im_end|> 這個 token
+                // 可能一開始就會收到 <|im_end|> 這個 token，因此要加上 index > 0 的判斷
+                // 可能會收不到 <|endoftext|>，反而是收收到 <|im_end|> 這個 token。
+                // 依設定檔來看，應該是兩個都要判斷。
+                // https://huggingface.co/Qwen/Qwen2.5-1.5B-Instruct/blob/main/generation_config.json
+                // "eos_token_id": [
+                //   151645,
+                //   151643
+                // ],
+                //println!("next_token:{}", next_token);
                 break;
             }
             if let Some(t) = self.tokenizer.next_token(next_token)? {
